39fb939e32266a9270218bd02b86c5babe3655bb
chaitanyakaranam <karanam.krishna.25@gmail.com>
2020-07-24 23:37:16 +0530

    Added dynamic validations

--- src/components/Forms/ControlledJobForm.js
+++ src/components/Forms/ControlledJobForm.js
@@ -2,7 +2,25 @@ import React, { useState } from 'react';
 
 function ControlledJobForm(props) {
 
-    let [ formData, setFormData ] = useState({})
+    let [ formData, setFormData ] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        location: '',
+        current_employer: '',
+        current_role: '',
+        role_description: '',
+        experience: 0,
+        skills: '',
+        portfolio_links: '',
+        social_media_links: ''
+    })
+
+    function handleOnSubmit(e){
+        e.preventDefault();
+        console.log(formData);
+    }
 
     function handleOnChange(field, value){
         let newFormData = { ...formData };
@@ -10,24 +28,29 @@ function ControlledJobForm(props) {
         setFormData(newFormData);
     }
 
+    console.log('render');
+
     return (
-        <form>
+        <form onSubmit={handleOnSubmit}>
             <section>
                 <h4>Personal Information</h4>
                 <br />
                 <div>
                     <label htmlFor="firstname">First Name</label>
-                    <input 
+                    <input
+                        required
                         type="text" 
                         id="firstname" 
                         name="firstname" 
-                        value={formData['firstname']} 
+                        value={formData['firstname'] ? formData['firstname'] : ''} 
                         onChange={e => handleOnChange('firstname', e.target.value)}
                     />
                 </div>
+                { formData['firstname'] === 'Admin' ? <span className="red-text">Cannot use this value</span> : <></> }
                 <div>
                     <label htmlFor="lastname">Last Name</label>
-                    <input 
+                    <input
+                        required
                         type="text" 
                         id="lastname" 
                         name="lastname" 
@@ -36,7 +59,8 @@ function ControlledJobForm(props) {
                 </div>
                 <div>
                     <label htmlFor="email">Email Address</label>
-                    <input 
+                    <input
+                        required 
                         type="email" 
                         id="email" 
                         name="email" 
@@ -45,16 +69,19 @@ function ControlledJobForm(props) {
                 </div>
                 <div>
                     <label htmlFor="phone">Phone Number</label>
-                    <input 
-                        type="text" 
+                    <input
+                        required
+                        type="tel" 
                         id="phone" 
-                        name="phone" 
+                        name="phone"
+                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
                         value={formData['phone']} 
                         onChange={e => handleOnChange('phone', e.target.value)}/>
                 </div>
                 <div>
                     <label htmlFor="location">Location</label>
-                    <input 
+                    <input
+                        required
                         type="text" 
                         id="location" 
                         name="location" 
@@ -77,7 +104,7 @@ function ControlledJobForm(props) {
                 </div>
                 <div>
                     <label htmlFor="current_role">Current Role</label>
-                    <input 
+                    <input
                         type="text" 
                         id="current_role" 
                         name="current_role" 
@@ -98,7 +125,7 @@ function ControlledJobForm(props) {
                 <div>
                     <label htmlFor="experience">Total Years of Experience</label>
                     <input 
-                        type="text" 
+                        type="number" 
                         id="experience" 
                         name="experience" 
                         value={formData['experience']} 
@@ -119,58 +146,68 @@ function ControlledJobForm(props) {
             <section>
                 <h4>Social Medial Links</h4>
                 <br />
+                <br />
                 <div>
+                    <label>
+                        <input
+                            className="filled-in"
+                            type="checkbox"
+                            name="include_portfolio"
+                            id="include_portfolio"
+                            onChange={e => handleOnChange('include_portfolio', e.target.checked)} />
+                        <span>Include Portfolio Links?</span>
+                    </label>
+                </div>
+                <br />
+                <div>
+                    <label>
+                        <input
+                            className="filled-in"
+                            type="checkbox"
+                            name="include_social"
+                            id="include_social"
+                            onChange={e => handleOnChange('include_social', e.target.checked)} />
+                        <span>Include Social Media Links?</span>
+                    </label>
+                </div>
+                <br />
+                { formData['include_portfolio'] && <div>
                     <label htmlFor="portfolio_links">Portfolio links</label>
-                    <input 
-                        type="text" 
+                    <input
+                        required 
+                        type="url" 
                         id="portfolio_links" 
                         name="portfolio_links" 
                         value={formData['portfolio_links']} 
                         onChange={e => handleOnChange('portfolio_links', e.target.value)}
                     />
-                </div>
-                <div>
+                </div>}
+                { formData['include_social'] && <div>
                     <label htmlFor="social_media_links">Social Media Links</label>
-                    <input 
-                        type="text" 
+                    <input
+                        required 
+                        type="url" 
                         id="social_media_links" 
                         name="social_media_links" 
                         value={formData['social_media_links']} 
                         onChange={e => handleOnChange('social_media_links', e.target.value)}
                     />
-                </div>
+                </div>}
             </section>
             <section>
                 <h4>Uploads</h4>
                 <br />
+                <br />
                 <div>
                     <label htmlFor="resume">Select Resume</label>
-                    <br/>
-                    <input type="file" id="resume"/>
+                    <br />
+                    <input type="file" id="resume" />
                 </div>
-                <br/><br/>
+                <br /><br />
                 <div>
                     <label htmlFor="cover_letter">Select Cover Letter</label>
-                    <br/>
-                    <input type="file" id="cover_letter"/>
-                </div>
-                <div className="file-field input-field">
-                    <div className="btn-small">
-                        <span>Upload Resume</span>
-                        <input type="file"/>
-                    </div>
-                    <div className="file-path-wrapper">
-                        <input className="file-path validate" type="text"/>
-                    </div>
-                </div>
-                <div className="file-field input-field">
-                    <div className="btn-small">
-                        <span>Upload Cover Letter</span>
-                        <input type="file"/>
-                    </div>
-                    <div className="file-path-wrapper">
-                        <input className="file-path validate" type="text"/>
-                    </div>
+                    <br />
+                    <input type="file" id="cover_letter" />
                 </div>
             </section>
             <br/><br/>
