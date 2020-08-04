7cfcea8bcc5c95998af8f23a51cad9b8af99d2b7
chaitanyakaranam <karanam.krishna.25@gmail.com>
2020-08-04 23:36:57 +0530

    Added dynamic json form

--- src/components/Forms/Reusable/Form.js
+++ src/components/Forms/Reusable/Form.js
@@ -1,123 +1,130 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 
-export function ReactForm() {
+function Form({ template, onSubmit, onError, watchFields=[], watchValues }) {
 
-    const { register, handleSubmit, watch } = useForm();
+    let { register, handleSubmit, errors, setError, clearErrors, watch, getValues } = useForm();
 
-    const onSubmit = (values) => console.log(values);
+    if (!template) {
+        return <span className="red-text">Need to pass template prop</span>
+    }
 
-    const watchValues = watch(['firstname', 'include_portfolio', 'include_social']);
+    let { title, sections } = template;
+
+    let wv = watch(watchFields);
+
+    watchValues(wv, { setError, clearErrors, errors });
+
+    const getField = (field) => {
+
+        let { title, type, name, id, validationProps, dynamic } = field;
+
+        let showField = dynamic ? getValues(dynamic['field']) === dynamic['value'] : true;
+
+        if(!showField) return;
+
+        switch (type) {
+            case 'text':
+                return (
+                    <div key={id}>
+                        <label htmlFor={id}>{title}</label>
+                        <input ref={register(validationProps)} type="text" name={name} id={id} />
+                        {errors[name] && <span className="red-text">{errors[name]['message']}</span>}
+                    </div>
+                )
+            case 'tel':
+                return (
+                    <div key={id}>
+                        <label htmlFor={id}>{title}</label>
+                        <input ref={register(validationProps)} type="tel" name={name} id={id} />
+                        {errors[name] && <span className="red-text">{errors[name]['message']}</span>}
+                    </div>
+                )
+            case 'number':
+                return (
+                    <div key={id}>
+                        <label htmlFor={id}>{title}</label>
+                        <input ref={register(validationProps)} type="number" name={name} id={id} />
+                        {errors[name] && <span className="red-text">{errors[name]['message']}</span>}
+                    </div>
+                )
+            case 'email':
+                return (
+                    <div key={id}>
+                        <label htmlFor={id}>{title}</label>
+                        <input ref={register(validationProps)} type="email" name={name} id={id} />
+                        {errors[name] && <span className="red-text">{errors[name]['message']}</span>}
+                    </div>
+                )
+            case 'url':
+                return (
+                    <div key={id}>
+                        <label htmlFor={id}>{title}</label>
+                        <input ref={register(validationProps)} type="url" name={name} id={id} />
+                        {errors[name] && <span className="red-text">{errors[name]['message']}</span>}
+                    </div>
+                )
+            case 'file':
+                return (
+                    <React.Fragment key={id}>
+                        <div>
+                            <label htmlFor={id}>{title}</label>
+                            <br/>
+                            <input ref={register(validationProps)} type="file" name={name} id={id} />
+                            {errors[name] && <span className="red-text">{errors[name]['message']}</span>}
+                        </div>
+                        <br/>
+                    </React.Fragment>
+                )
+            case 'checkbox':
+                return (
+                    <div key={id}>
+                        <label>
+                            <input className="filled-in" ref={register(validationProps)} type="checkbox" name={name} id={id} />
+                            <span>{title}</span>
+                        </label>
+                        {errors[name] && <span className="red-text">{errors[name]['message']}</span>}
+                    </div>
+                )
+            default:
+                return <span key="invalid" className="red-text">Invalid field detected</span>
+        }
+    }
+
+    const renderFields = (fields) => {
+        if (fields && Array.isArray(fields)) {
+            return fields.map(field => getField(field))
+        } else {
+            return <span className="red-text">Invalid Sections in template</span>
+        }
+    }
+
+    const renderSections = (sections) => {
+        if (sections && Array.isArray(sections)) {
+            return sections.map(({ title, id, fields }) => {
+                return (
+                    <section key={id}>
+                        <h5>{title}</h5>
+                        <br />
+                        {renderFields(fields)}
+                    </section>
+                )
+            })
+        } else {
+            return <span className="red-text">Invalid Sections in template</span>
+        }
+    }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            {console.log('render')}
-            <section>
-                <h4>Personal Information</h4>
-                <br />
-                <div>
-                    <label htmlFor="firstname">First Name</label>
-                    <input ref={register} type="text" id="firstname" name="firstname" />
-                    {watchValues && watchValues['firstname'] === 'Admin' ? <span>You cannot use this</span> : <></>}
-                </div>
-                <div>
-                    <label htmlFor="lastname">Last Name</label>
-                    <input ref={register} type="text" id="lastname" name="lastname" />
-                </div>
-                <div>
-                    <label htmlFor="email">Email Address</label>
-                    <input ref={register} type="email" id="email" name="email" />
-                </div>
-                <div>
-                    <label htmlFor="phone">Phone Number</label>
-                    <input ref={register} type="text" id="phone" name="phone" />
-                </div>
-                <div>
-                    <label htmlFor="location">Location</label>
-                    <input ref={register} type="text" id="location" name="location" />
-                </div>
-            </section>
-            <section>
-                <h4>Work Information</h4>
-                <br />
-                <div>
-                    <label htmlFor="current_employer">Current Employer</label>
-                    <input ref={register} type="text" id="current_employer" name="current_employer" />
-                </div>
-                <div>
-                    <label htmlFor="current_role">Current Role</label>
-                    <input ref={register} type="text" id="current_role" name="current_role" />
-                </div>
-                <div>
-                    <label htmlFor="role_description">Role Description</label>
-                    <input ref={register} type="text" id="role_description" name="role_description" />
-                </div>
-                <div>
-                    <label htmlFor="experience">Total Years of Experience</label>
-                    <input ref={register} type="text" id="experience" name="experience" />
-                </div>
-                <div>
-                    <label htmlFor="skills">Skills</label>
-                    <input ref={register} type="text" id="skills" name="skills" />
-                </div>
-            </section>
-            <section>
-                <h4>Social Medial Links</h4>
-                <br />
-                <div>
-                    <label>
-                        <input
-                            ref={register}
-                            className="filled-in"
-                            type="checkbox"
-                            name="include_portfolio"
-                            id="include_portfolio" />
-                        <span>Include Portfolio Links?</span>
-                    </label>
-                </div>
-                <br />
-                <div>
-                    <label>
-                        <input
-                            ref={register}
-                            className="filled-in"
-                            type="checkbox"
-                            name="include_social"
-                            id="include_social" />
-                        <span>Include Social Media Links?</span>
-                    </label>
-                </div>
-                <br />
-                {watchValues && watchValues['include_portfolio'] && <div>
-                    <label htmlFor="portfolio_links">Portfolio links</label>
-                    <input ref={register} type="text" id="portfolio_links" name="portfolio_links" />
-                </div>}
-                {watchValues && watchValues['include_social'] && <div>
-                    <label htmlFor="social_media_links">Social Media Links</label>
-                    <input ref={register} type="text" id="social_media_links" name="social_media_links" />
-                </div>}
-
-            </section>
-            <section>
-                <h4>Uploads</h4>
-                <br />
-                <div>
-                    <label htmlFor="resume">Select Resume</label>
-                    <br />
-                    <input type="file" id="resume" />
-                </div>
-                <br /><br />
-                <div>
-                    <label htmlFor="cover_letter">Select Cover Letter</label>
-                    <br />
-                    <input type="file" id="cover_letter" />
-                </div>
-            </section>
-            <br /><br />
-            <button type="submit" className="btn">Submit Form</button>
+            <h4>{title}</h4>
+            <div className="divider"></div>
+            <br />
+            {renderSections(sections)}
+            <br/>
+            <button type="submit" className="btn" onClick={() => onError(errors)}>Submit Form</button>
         </form>
-    )
+    );
 }
 
-
-export default ReactForm;
+export default Form;
