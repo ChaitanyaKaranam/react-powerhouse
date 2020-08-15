0cfd1b06d817226d0c6ad188239159ea5e250f52
chaitanyakaranam <karanam.krishna.25@gmail.com>
2020-08-15 22:42:07 +0530

    Updates

---
+++ precache-manifest.0b9ca6598ef67f66a1a2d8571c38f7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc45dafe8ebe20728fd791e891433a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7be8099a6169c37e1298",
+    "url": "./static/css/main.f4969396.chunk.css"
+  },
+  {
+    "revision": "775de3fc654a606c7d25",
+    "url": "./static/js/2.c6a7b53d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.c6a7b53d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be8099a6169c37e1298",
+    "url": "./static/js/main.497e2402.chunk.js"
+  },
+  {
+    "revision": "43c067358a765905c005",
+    "url": "./static/js/runtime-main.e074de18.js"
+  }
+]);
