ca2faebb6919a4cbf3f18b1448a7c6982f21aaa0
chaitanyakaranam <karanam.krishna.25@gmail.com>
2020-08-15 20:13:27 +0530

    Updates

---
+++ precache-manifest.fc311ff337e9a1344622bd446509989d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ab44be184df1ba58dea2847bf2975a",
+    "url": "/ChaitanyaKaranam/react-powerhouse/index.html"
+  },
+  {
+    "revision": "94a6ecb5e4aa78cce9db",
+    "url": "/ChaitanyaKaranam/react-powerhouse/static/css/main.24a9d934.chunk.css"
+  },
+  {
+    "revision": "775de3fc654a606c7d25",
+    "url": "/ChaitanyaKaranam/react-powerhouse/static/js/2.c6a7b53d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ChaitanyaKaranam/react-powerhouse/static/js/2.c6a7b53d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a6ecb5e4aa78cce9db",
+    "url": "/ChaitanyaKaranam/react-powerhouse/static/js/main.6fe9c182.chunk.js"
+  },
+  {
+    "revision": "80eb10b77dcedabc9ed4",
+    "url": "/ChaitanyaKaranam/react-powerhouse/static/js/runtime-main.14d33b66.js"
+  }
+]);
